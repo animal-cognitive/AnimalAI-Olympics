9e601687b52d06e66b43b0fa32d458c927314fbe
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2021-03-24 12:30:19 -0500

    Add sanity test for CNN-LSTM model. Fix some shapes. Add "gimme_lstm" flag to enable/disable LSTM.

--- cachey/cache_model.py
+++ cachey/cache_model.py
@@ -3,12 +3,16 @@ Custom models specific to Cache architecture.
 """
 
 import operator
+import unittest
 from functools import reduce
 
+import gym
 import numpy as np
 import torch
 import torch.nn.functional as F
+from ray.rllib.env.wrappers.atari_wrappers import wrap_deepmind
 from ray.rllib.models import ModelV2
+from ray.rllib.models.preprocessors import get_preprocessor
 from ray.rllib.models.torch.recurrent_net import RecurrentNetwork
 from ray.rllib.policy.rnn_sequencing import add_time_dimension
 from ray.rllib.utils import override
@@ -27,10 +31,17 @@ class MyCNNRNNModel(RecurrentNetwork, nn.Module):
         assert len(obs_space.shape) == 2 or len(obs_space.shape) == 3, \
             f'MyCNNRNNModel must take image observations!'
 
-        if len(obs_space.shape) < 4:
+        if len(obs_space.shape) == 2:
+            w, h = obs_space.shape
             in_channels = 1  # Assume BW input
         else:
-            in_channels = obs_space.shape[0]
+            w, h, in_channels = obs_space.shape
+
+        # By default, use LSTM. But leave an option to disable it.
+        if "gimme_lstm" not in model_config:
+            model_config["gimme_lstm"] = True
+        self.use_lstm = model_config["gimme_lstm"]
+
         # Nature CNN (Mnih et. al 2015)
         self.cnn = nn.Sequential(
             nn.Conv2d(in_channels, 32, kernel_size=8, stride=4, padding=0),
@@ -41,29 +52,35 @@ class MyCNNRNNModel(RecurrentNetwork, nn.Module):
             nn.ReLU(),
             nn.Flatten(),
         )
-
         # Do a forward pass of dummy data to get the shape of the CNN output
         with torch.no_grad():
             dummy_input = torch.zeros(obs_space.shape)
-            if len(obs_space.shape) < 4:
-                dummy_input = dummy_input.unsqueeze(0).unsqueeze(0)
+            if len(obs_space.shape) == 2:
+                dummy_input = dummy_input.unsqueeze(2)
+            dummy_input = dummy_input.permute(2, 0, 1)
+            dummy_input = dummy_input.unsqueeze(0)  # Add batch dim
             dummy_output = self.cnn(dummy_input)
             self.cnn_output_flat_size = reduce(operator.mul, dummy_output.shape[1:])
 
-        # Build the Module from fc + LSTM + 2xfc (action + value outs).
-        # Parameter lstm_state_size configures the size of the hidden state.
-        if not model_config.get("lstm_state_size"):
-            model_config["lstm_state_size"] = 256
         if not model_config.get("linear_size"):
             model_config["linear_size"] = 64
-        self.lstm_state_size = model_config["lstm_state_size"]
         self.linear_size = model_config["linear_size"]
-
         self.linear = nn.Linear(self.cnn_output_flat_size, self.linear_size)
-        self.lstm = nn.LSTM(self.linear_size, self.lstm_state_size, batch_first=True)
 
-        self.action_branch = nn.Linear(self.lstm_state_size, num_outputs)
-        self.value_branch = nn.Linear(self.lstm_state_size, 1)
+        if self.use_lstm:
+            # Build the Module from fc + LSTM + 2xfc (action + value outs).
+            # Parameter lstm_state_size configures the size of the hidden state.
+            if not model_config.get("lstm_state_size"):
+                model_config["lstm_state_size"] = 256
+            self.lstm_state_size = model_config["lstm_state_size"]
+
+            self.lstm = nn.LSTM(self.linear_size, self.lstm_state_size, batch_first=True)
+            n_logits = self.lstm_state_size
+        else:
+            n_logits = self.linear_size
+
+        self.action_branch = nn.Linear(n_logits, num_outputs)
+        self.value_branch = nn.Linear(n_logits, 1)
 
         # Holds the last "base" output (before logits layer) for calls to value_function.
         self._features = None
@@ -92,28 +109,34 @@ class MyCNNRNNModel(RecurrentNetwork, nn.Module):
         # Pass observation to CNN
         # The default RecurrentNetwork flattens the observation first.
         # We want to preserve spatials.
-        obs = input_dict["obs"].float()
-        if len(obs.shape) < 4:
-            obs = obs.unsqueeze(dim=1).float()  # Add channel dim
-        obs = self.cnn(obs)
-        obs = F.relu(self.linear(obs))
+        x = input_dict["obs"].float()
+        if len(x.shape[1:]) < 3:
+            x = x.unsqueeze(dim=1).float()  # Add channel dim
+        x = self.cnn(x)
+        x = F.relu(self.linear(x))
 
-        # Add time dimension
-        if isinstance(seq_lens, np.ndarray):
-            seq_lens = torch.Tensor(seq_lens).int()
-        max_seq_len = obs.shape[0] // seq_lens.shape[0]
-        self.time_major = self.model_config.get("_time_major", False)
-        obs = add_time_dimension(
-            obs,
-            max_seq_len=max_seq_len,
-            framework="torch",
-            time_major=self.time_major,
-        )
+        if self.use_lstm:
+            # Add time dimension
+            if isinstance(seq_lens, np.ndarray):
+                seq_lens = torch.Tensor(seq_lens).int()
+            max_seq_len = x.shape[0] // seq_lens.shape[0]
+            self.time_major = self.model_config.get("_time_major", False)
+            x = add_time_dimension(
+                x,
+                max_seq_len=max_seq_len,
+                framework="torch",
+                time_major=self.time_major,
+            )
 
-        # Pass to LSTM and out
-        output, new_state = self.forward_rnn(obs, state, seq_lens)
-        output = torch.reshape(output, [-1, self.num_outputs])
-        return output, new_state
+            # Pass to LSTM and out
+            self._features, state = self.forward_rnn(x, state, seq_lens)
+        else:
+            self._features = x
+
+        action_out = self.action_branch(self._features)
+        action_out = torch.reshape(action_out, [-1, self.num_outputs])  # Remove time dim if present
+
+        return action_out, state
 
     @override(RecurrentNetwork)
     def forward_rnn(self, inputs, state, seq_lens):
@@ -123,8 +146,43 @@ class MyCNNRNNModel(RecurrentNetwork, nn.Module):
             NN Outputs (B x T x ...) as sequence.
             The state batches as a List of two items (c- and h-states).
         """
-        self._features, [h, c] = self.lstm(
+        output, [h, c] = self.lstm(
             inputs, [torch.unsqueeze(state[0], 0),
                      torch.unsqueeze(state[1], 0)])
-        action_out = self.action_branch(self._features)
-        return action_out, [torch.squeeze(h, 0), torch.squeeze(c, 0)]
+        return output, [torch.squeeze(h, 0), torch.squeeze(c, 0)]
+
+
+def get_model(env_id, options):
+    env = gym.make(env_id)
+    env = wrap_deepmind(env, framestack=False)
+    obs_space = env.observation_space
+    action_space = env.action_space
+    num_outputs = env.action_space.n
+    model_config = options
+    name = 'test'
+    prep = get_preprocessor(env.observation_space)(env.observation_space)
+    obs = torch.cat(
+        [torch.from_numpy(prep.transform(env.observation_space.sample())).permute(2, 0, 1) for _ in range(100)],
+        dim=0)
+    input_dict = {
+        "obs": obs,
+    }
+    model = MyCNNRNNModel(obs_space, action_space, num_outputs, model_config, name)
+    return model, input_dict
+
+
+class TestMyCNNLSTMModel(unittest.TestCase):
+
+    def test_sanity_cnn_only(self):
+        model, input_dict = get_model('PongNoFrameskip-v4', {'gimme_lstm': False})
+        model(input_dict)
+
+    def test_sanity_cnn_lstm(self):
+        model, input_dict = get_model('Pong-v4', {})
+        state = [s.unsqueeze(0) for s in model.get_initial_state()]
+        seq_lens = torch.tensor([input_dict["obs"].shape[0]])  # One sequence of the whole thing
+        model(input_dict, state, seq_lens)
+
+
+if __name__ == '__main__':
+    unittest.main()
