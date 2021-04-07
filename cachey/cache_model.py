af804aa961fac1baed6e587a88c7aaa26e14dcba
Benjamin Steenhoek <benjaminjsteenhoek@gmail.com>
2021-04-07 15:30:29 -0500

    Separate setup functions in Cache model

--- cachey/cache_model.py
+++ cachey/cache_model.py
@@ -14,7 +14,6 @@ import torch
 import torch.nn.functional as F
 from animalai.envs.arena_config import ArenaConfig
 from ray.rllib.agents.ppo import PPOTrainer
-from ray.rllib.env.atari_wrappers import wrap_deepmind
 from ray.rllib.models import ModelV2, ModelCatalog
 from ray.rllib.models.preprocessors import get_preprocessor
 from ray.rllib.models.torch.recurrent_net import RecurrentNetwork
@@ -23,7 +22,7 @@ from ray.rllib.utils import override
 from ray.tune import register_env
 from torch import nn
 
-from cognitive.primitive_arena import RayAIIGym
+from cachey.convlstmgru.convgru import ConvGRU
 
 
 class MyCNNRNNModel(RecurrentNetwork, nn.Module):
@@ -38,7 +37,7 @@ class MyCNNRNNModel(RecurrentNetwork, nn.Module):
     ...
     "custom_model": 'my_cnn_rnn_model',
     "custom_model_config": {
-        'gimme_lstm': True|False
+        'enable_lstm': True|False
     }
     }
     """
@@ -56,11 +55,48 @@ class MyCNNRNNModel(RecurrentNetwork, nn.Module):
         else:
             w, h, in_channels = obs_space.shape
 
-        # By default, use LSTM. But leave an option to disable it.
-        if "gimme_lstm" not in model_config:
-            model_config["gimme_lstm"] = True
-        self.use_lstm = model_config["gimme_lstm"]
+        self._setup_config(model_config)
 
+        dummy_cnn_output = self._setup_cnn(in_channels, obs_space.shape)
+        cnn_output_flat_size = reduce(operator.mul, dummy_cnn_output.shape[1:])
+        self.linear = nn.Linear(cnn_output_flat_size, self.linear_size)
+        lstm_input_size = self.linear
+        n_logits = self.linear_size
+
+        if self.use_convgru:
+            dummy_convgru_output = self._setup_convgru(dummy_cnn_output.shape)
+            convgru_output_flat_size = reduce(operator.mul, dummy_convgru_output[2:])
+            lstm_input_size = convgru_output_flat_size
+            n_logits = convgru_output_flat_size
+
+        if self.use_lstm:
+            # Build the Module from fc + LSTM + 2xfc (action + value outs).
+            # Parameter lstm_state_size configures the size of the hidden state.
+            self.lstm = nn.LSTM(lstm_input_size, self.lstm_state_size, batch_first=True)
+            n_logits = self.lstm_state_size
+
+        self.action_branch = nn.Linear(n_logits, num_outputs)
+        self.value_branch = nn.Linear(n_logits, 1)
+
+        # Holds the last "base" output (before logits layer) for calls to value_function.
+        self._features = None
+
+    def _setup_config(self, model_config):
+        if "enable_convgru" not in model_config:
+            model_config["enable_convgru"] = True
+        self.use_convgru = model_config["enable_convgru"]
+        # By default, use LSTM. But leave an option to disable it.
+        if "enable_lstm" not in model_config:
+            model_config["enable_lstm"] = True
+        self.use_lstm = model_config["enable_lstm"]
+        if not model_config.get("lstm_state_size"):
+            model_config["lstm_state_size"] = 256
+        self.lstm_state_size = model_config["lstm_state_size"]
+        if not model_config.get("linear_size"):
+            model_config["linear_size"] = 64
+        self.linear_size = model_config["linear_size"]
+
+    def _setup_cnn(self, in_channels, input_shape):
         # Nature CNN (Mnih et. al 2015)
         self.cnn = nn.Sequential(
             nn.Conv2d(in_channels, 32, kernel_size=8, stride=4, padding=0),
@@ -69,48 +105,50 @@ class MyCNNRNNModel(RecurrentNetwork, nn.Module):
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
             nn.ReLU(),
-            nn.Flatten(),
         )
+        self.cnn_flatten = nn.Flatten()
         # Do a forward pass of dummy data to get the shape of the CNN output
         with torch.no_grad():
-            dummy_input = torch.zeros(obs_space.shape)
-            if len(obs_space.shape) == 2:
+            dummy_input = torch.zeros(input_shape)
+            if len(input_shape) == 2:
                 dummy_input = dummy_input.unsqueeze(2)
             dummy_input = dummy_input.permute(2, 0, 1)
             dummy_input = dummy_input.unsqueeze(0)  # Add batch dim
             dummy_output = self.cnn(dummy_input)
-            self.cnn_output_flat_size = reduce(operator.mul, dummy_output.shape[1:])
+        return dummy_output
 
-        if not model_config.get("linear_size"):
-            model_config["linear_size"] = 64
-        self.linear_size = model_config["linear_size"]
-        self.linear = nn.Linear(self.cnn_output_flat_size, self.linear_size)
+    def _setup_convgru(self, input_shape):
+        input_size = input_shape[2:]
+        input_dim = input_shape[1]
+        hidden_dims = [128, 128, 128]
+        kernel_size = (3, 3)
+        num_layers = 3
+        self.convgru = ConvGRU(input_size, input_dim, hidden_dims, kernel_size, num_layers, batch_first=True)
+        self.convgru_flat = nn.Flatten(start_dim=2)
+        with torch.no_grad():
+            dummy_input = torch.zeros(input_shape).unsqueeze(0)
+            dummy_gru_state = self.get_gru_initial_state()
+            dummy_output, _ = self.convgru(dummy_input, [h.unsqueeze(0) for h in dummy_gru_state])
+        return dummy_output
 
-        if self.use_lstm:
-            # Build the Module from fc + LSTM + 2xfc (action + value outs).
-            # Parameter lstm_state_size configures the size of the hidden state.
-            if not model_config.get("lstm_state_size"):
-                model_config["lstm_state_size"] = 256
-            self.lstm_state_size = model_config["lstm_state_size"]
-
-            self.lstm = nn.LSTM(self.linear_size, self.lstm_state_size, batch_first=True)
-            n_logits = self.lstm_state_size
-        else:
-            n_logits = self.linear_size
-
-        self.action_branch = nn.Linear(n_logits, num_outputs)
-        self.value_branch = nn.Linear(n_logits, 1)
-
-        # Holds the last "base" output (before logits layer) for calls to value_function.
-        self._features = None
+    def get_gru_initial_state(self):
+        gru_h = self.convgru.get_init_states(1, False)
+        gru_h = [h.squeeze(0) for h in gru_h]
+        return gru_h
 
     @override(ModelV2)
     def get_initial_state(self):
-        h = [
-            self.linear.weight.new(1, self.lstm_state_size).zero_().squeeze(0),
-            self.linear.weight.new(1, self.lstm_state_size).zero_().squeeze(0)
-        ]
-        return h
+        state = []
+        if self.use_convgru:
+            gru_h = self.get_gru_initial_state()
+            state += gru_h
+        if self.use_lstm:
+            lstm_h = [
+                self.linear.weight.new(1, self.lstm_state_size).zero_().squeeze(0),
+                self.linear.weight.new(1, self.lstm_state_size).zero_().squeeze(0)
+            ]
+            state += lstm_h
+        return state
 
     @override(ModelV2)
     def value_function(self):
@@ -133,9 +171,11 @@ class MyCNNRNNModel(RecurrentNetwork, nn.Module):
             x = x.unsqueeze(dim=3).float()  # Add channel dim
         x = x.permute(0, 3, 1, 2)  # Permute to (B x C x W x H)
         x = self.cnn(x)
-        x = F.relu(self.linear(x))
 
-        if self.use_lstm:
+        if self.use_convgru or self.use_lstm:
+            if not self.use_convgru:
+                x = self.cnn_flatten(x)
+                x = F.relu(self.linear(x))
             # Add time dimension
             if isinstance(seq_lens, np.ndarray):
                 seq_lens = torch.Tensor(seq_lens).int()
@@ -147,38 +187,53 @@ class MyCNNRNNModel(RecurrentNetwork, nn.Module):
                 framework="torch",
                 time_major=self.time_major,
             )
-
-            # Pass to LSTM and out
-            self._features, state = self.forward_rnn(x, state, seq_lens)
+            x, state = self.forward_rnn(x, state, seq_lens)
         else:
-            self._features = x
+            x = self.cnn_flatten(x)
+            x = F.relu(self.linear(x))
 
+        self._features = x
         action_out = self.action_branch(self._features)
         action_out = torch.reshape(action_out, [-1, self.num_outputs])  # Remove time dim if present
 
         return action_out, state
 
     @override(RecurrentNetwork)
-    def forward_rnn(self, inputs, state, seq_lens):
+    def forward_rnn(self, x, state, seq_lens):
         """Feeds `inputs` (B x T x ..) through the Gru Unit.
         Returns the resulting outputs as a sequence (B x T x ...).
         Returns:
             NN Outputs (B x T x ...) as sequence.
             The state batches as a List of two items (c- and h-states).
         """
-        output, [h, c] = self.lstm(
-            inputs, [torch.unsqueeze(state[0], 0),
-                     torch.unsqueeze(state[1], 0)])
-        return output, [torch.squeeze(h, 0), torch.squeeze(c, 0)]
+
+        new_state = []
+
+        if self.use_convgru:
+            gru_state = state[:-2]
+            x, gru_state = self.convgru(x, gru_state)
+            new_state += gru_state
+            x = self.convgru_flat(x)
+
+        if self.use_lstm:
+            lstm_state = state[-2:]
+            x, lstm_state = self.lstm(x, [s.unsqueeze(0) for s in lstm_state])
+            lstm_state = [s.squeeze(0) for s in lstm_state]
+            new_state += lstm_state
+
+        return x, new_state
 
 
 @pytest.mark.parametrize('model_config', [
-    {'gimme_lstm': False},  # CNN only
-    {'gimme_lstm': True},  # CNN+LSTM
+    {'enable_lstm': True, 'enable_convgru': True},  # CNN+ConvGRU+LSTM
+    # {'enable_lstm': False, 'enable_convgru': True},  # CNN+ConvGRU
+    {'enable_lstm': True, 'enable_convgru': False},  # CNN+LSTM
+    {'enable_convgru': False, 'enable_lstm': False},  # CNN only
     {},  # Also CNN+LSTM, just checking that the defaults are sane
 ])
 def test_sanity(model_config):
     """Test simple passing observations."""
+    from ray.rllib.env.atari_wrappers import wrap_deepmind
     env = wrap_deepmind(gym.make('Pong-v4'))
     prep = get_preprocessor(env.observation_space)(env.observation_space)
     obs = torch.stack([torch.from_numpy(prep.transform(env.observation_space.sample())) for _ in range(100)], dim=0)
@@ -186,17 +241,16 @@ def test_sanity(model_config):
         "obs": obs,
     }
     model = MyCNNRNNModel(env.observation_space, env.action_space, env.action_space.n, model_config, 'test')
-    if model.use_lstm:
-        state = [s.unsqueeze(0) for s in model.get_initial_state()]
-        seq_lens = torch.tensor([input_dict["obs"].shape[0]])  # One sequence of the whole thing
-        model(input_dict, state, seq_lens)
-    else:
-        model(input_dict)
+    state = model.get_initial_state()
+    state = [s.unsqueeze(0) for s in state]
+    seq_lens = torch.tensor([input_dict["obs"].shape[0]])  # One sequence of the whole thing
+    model(input_dict, state, seq_lens)
     env.close()
 
 
 def test_trainonebatch():
     """Test hooking into an RLLib trainer."""
+    from cognitive.primitive_arena import RayAIIGym
     ray.shutdown()
     ray.init(include_dashboard=False)
     register_env("my_env", lambda c: RayAIIGym(c, ArenaConfig('examples/configurations/curriculum/0.yml')))
